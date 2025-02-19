055634b8f11157ee5c9bec805c5a84e1bab03201
vedantsen905 <120441464+vedantsen905@users.noreply.github.com>
2025-02-19 16:22:38 +0530

    New project added

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+  // let counter = 5
+  const removeValue = () => { 
+    if (counter > 0) {
+      setCounter(counter - 1);
+      
+      }
+    
+  }
+
+  const addValue = () => {
+    console.log("valuem added", counter );
+    if (counter <  20) {
+      setCounter(counter + 1);
+    }
+    else {
+      console.log("value is greater than 20");
+      ;
+    }
+    }
+
+ 
+  
+  return (
+    <>
+    <h1>Counter aur react  </h1>
+    <h2>counter value: {counter}</h2>
+    <button onClick= {addValue}>
+      Add Value</button> <br></br><br></br>
+
+    <button onClick= {removeValue}>removeValue </button>
+      </> )
+} 
+
+export default App
--- 02_counter/src/main.jsx
+++ 02_counter/src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
- 
+import './index.css'
 import App from './App.jsx'
- 
 
-function MyApp(){
-  return ( <h1>VEDANT chai </h1>)
-}
 createRoot(document.getElementById('root')).render(
-
-  < MyApp  />  
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
-
-   
